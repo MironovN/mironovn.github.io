4c99d4fb762f2ffca70e89f9d830e7ad792d8966
MironovN <30507246+MironovN@users.noreply.github.com>
2022-10-28 20:06:39 +0300

    Add files via upload

---
+++ IT/script/script.js
@@ -0,0 +1,11 @@
+		function scrollButton1(){
+			window.scrollTo(0, window.innerHeight + 2)}
+		
+		function scrollButton2(){
+			window.scrollTo(0, window.innerHeight * 2 + 2)}
+		
+		function cont(){
+			window.scrollTo(0, window.innerHeight * 4 + 2)}
+		
+		function gall(){
+			window.scrollTo(0, window.innerHeight * 3 + 2)}
