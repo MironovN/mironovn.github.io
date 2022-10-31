dbaef202161f9fa5af89da9e5d4e9efafd838b0e
MironovN <30507246+MironovN@users.noreply.github.com>
2022-10-31 19:08:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-		function scrollButton1(){
-			window.scrollTo(0, window.innerHeight + 2)}
+function scrollButton1(){
+			window.scrollTo(0, window.innerHeight + 30)}
 		
 		function scrollButton2(){
-			window.scrollTo(0, window.innerHeight * 2 + 2)}
+			window.scrollTo(0, window.innerHeight * 2 - 10)}
 		
 		function cont(){
-			window.scrollTo(0, window.innerHeight * 4 + 2)}
+			window.scrollTo(0, window.innerHeight * 4 + 30)}
 		
 		function gall(){
-			window.scrollTo(0, window.innerHeight * 3 + 2)}
+			window.scrollTo(0, window.innerHeight * 3 + 30)}
