e73ea9a5dbf58bb7f7794cbe77050e01d712b42a
MironovN <30507246+MironovN@users.noreply.github.com>
2022-11-01 18:17:20 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function scrollButton1(){
-			window.scrollTo(0, window.innerHeight + 30)}
+		function scrollButton1(){
+			window.scrollTo(0, window.innerHeight - (0.05 * window.innerHeight))}
 		
 		function scrollButton2(){
 			window.scrollTo(0, window.innerHeight * 2 - 10)}
